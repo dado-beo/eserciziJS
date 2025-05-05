cb08dd3e8eac8c354ac5f0a976513d4e6a5400c7
dado-beo <123021686+dado-beo@users.noreply.github.com>
2025-05-05 12:37:10 +0200

    Add files via upload

---
+++ oggettoLibro.js
@@ -0,0 +1,11 @@
+let libro = {
+    titolo: "Il Signore degli Anelli",
+    autore: "J.R.R. Tolkien",
+    anno: 1954,
+    descrizione: function() {
+        return `Titolo: ${this.titolo}, Autore: ${this.autore}, Anno: ${this.anno}`;
+    }
+};
+
+// Esempio di utilizzo:
+console.log(libro.descrizione());
